86bb5d38c26e513022b9441206a5d257c8543b49
cbayonao <drcamilob@live.com>
2020-02-06 14:59:40 -0500

    ultimo rama html

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,23 +1,22 @@
 import scrapy
 """
-First class to do webscrapper
+Class for webscrapping rama judicial
 """
 
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-    ]
+
+    def start_requests(self):
+        urls = [
+            'https://consultaprocesos.ramajudicial.gov.co/Procesos/NumeroRadicacion',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
-            }
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
-            for href in response.css('li.next a::attr(href)'):
-                yield response.follow(next_page, callback=self.parse)
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
